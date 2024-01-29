1a7229c29afc331c3f6ec3f3dc2a12c608b3fc56
Andrew Boyer <asboyer@gmail.com>
2024-01-29 17:18:54 -0500

    init

---
+++ app.py
@@ -0,0 +1,64 @@
+from flask import Flask, render_template
+from datetime import datetime
+import os, json
+
+import goat_grade as gg
+
+app = Flask(__name__)
+root = os.path.dirname(__file__)
+
+# MAP OUT THIS WHOLE APP
+# PERFECT ALGORITHM / RANKING
+# MAKE ALL TIME RANKING
+# ADD TEAM STATS
+# WORK ON DISPLAY
+
+@app.route("/")
+def index():
+    text = """
+    goat grade raw data
+    """
+
+    return text.strip()
+
+@app.route("/stats/<name>")
+def data(name):
+
+    if os.path.exists(os.path.join(root, f'stats/{name}')):
+
+        today = datetime.today()
+        current_year = today.year
+        current_month = today.month
+        current_day = today.day
+
+        year = int(name.split(".")[0])
+
+        in_season = current_month in gg.NBA_SEASON
+        current_year_og = current_month in [10, 11, 12] and current_year + 1 == year
+        current_year_alt = current_month in [1, 2, 3, 4, 5, 6] and current_year == year
+        
+        if in_season and (current_year_og or current_year_alt) and last_day != current_day:
+            get_stats(year)
+            f = open(os.path.join(root, f'stats/{name}'))
+            data = json.load(f)
+            return data
+        
+        else:
+            f = open(os.path.join(root, f'stats/{name}'))
+            data = json.load(f)
+            return data
+    else:
+        
+        try:
+            year = int(name.split(".")[0])
+        except:
+            return "Invalid path"
+        
+        try:
+            get_stats(year)
+            f = open(os.path.join(root, f'stats/{name}'))
+            data = json.load(f)
+            return data
+        except TypeError:
+            return f"{year} is not a valid season!"
+
